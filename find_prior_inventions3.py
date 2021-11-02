b9b83abeac3069a20d369c62ee397ff5e2cbc08e
Shawn Hymel <hymelsr@vt.edu>
2021-11-02 10:18:57 -0500

    Added version to work with Python 3
    I know this is an old project, but I found it very helpful. I made a PR to add a version that works with Python 3.

---
+++ find_prior_inventions3.py
@@ -0,0 +1,25 @@
+# Update to work with Python 3
+import json
+import sys
+from urllib.request import urlopen
+from datetime import datetime
+
+def fmt_dt(ds):
+    """Accepts wc3 time and prints utc date."""
+    dt =  datetime.strptime(ds, "%Y-%m-%dT%H:%M:%SZ")
+    return dt.strftime('%Y-%m-%d')
+
+
+url = "https://api.github.com/users/%s/repos?type=owner&sort=created" % sys.argv[1]
+repos = json.loads(
+    urlopen(url).read()
+)
+
+output = [(repo['name'], fmt_dt(repo['created_at']), repo['html_url']) for repo in repos]
+
+biggest_title = max(map(lambda x: len(x[0]), output))
+
+format_str = "%%-%ds\t%%s\t%%s" % (biggest_title)
+print(format_str)
+for attributes in output:
+    print(format_str % attributes)
